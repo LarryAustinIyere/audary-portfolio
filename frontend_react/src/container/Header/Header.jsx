5cd762d37d218242979afbb13df62d0ef9312f3e
LarryAustinIyere <austinlarry1506@gmail.com>
2022-12-19 01:11:28 +0100

    updated header

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -26,6 +26,7 @@ const Header = () => {
             >
                 <div className='app__header-badge'>
                     <div className='badge-cmp app__flex'>
+                        <span>👋</span>
                         <div style={{ marginLeft: 20 }}>
                             <p className='p-text'>Hello, I am</p>
                             <h1 className='head-text'>Larry Austin Iyere</h1>
