01875ad43e73dd2b1a39f155f31582e748d51cd8
LarryAustinIyere <austinlarry1506@gmail.com>
2022-12-28 22:58:06 +0100

    Update Work.jsx

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -39,7 +39,7 @@ const Work = () => {
     }
     return (
         <>
-            <h2 className='head-text'> My Creative <span> Portfolio </span> </h2>
+            <h2 className='head-text'> My <span> Portfolio </span> </h2>
             <div className='app__work-filter'>
                 {['UI/UX', 'Web App', 'Next js', 'React js', 'All'].map((item, index) => (
                     <div
